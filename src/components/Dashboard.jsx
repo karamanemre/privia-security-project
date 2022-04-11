65113036d4e2d0cb8f4f047faba8de1f8b00d1bb
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-04-12 00:04:00 +0300

    Dashboard added

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route, Routes } from 'react-router'
+import HomePage from '../pages/HomePage'
+
+function Dashboard() {
+  return (
+    <div>
+        <Routes>
+            <Route path='/' element={<HomePage/>} />
+        </Routes>
+    </div>
+  )
+}
+
+export default Dashboard
