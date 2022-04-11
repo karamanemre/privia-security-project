d422c89b0ca19f67712b3bec4ea23cbc5156f7f8
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-04-12 00:04:22 +0300

    Navbar component added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Button from "./Button";
+import Logo from "./Logo";
+
+function Navbar() {
+  return (
+    <nav className="navbar d-flex justify-content-beetween ">
+      <div className="logo-container">
+        <Logo src={"./img/Privia_logo.png"} height={"41px"} width={"145px"} />
+      </div>
+      <div className="items">
+        <ul className="d-flex justify-content-center">
+          <li>Home</li>
+          <li>Home</li>
+        </ul>
+      </div>
+      <div className="button-container d-flex justify-content-center align-items-center">
+        <div>
+          <Button
+            text={"Login"}
+            className={"btn btn-link"}
+            width={"auto"}
+            height={"25px"}
+          />
+        </div>
+        <div className="">
+          <Button
+            text={"Start free trial"}
+            className={"btn btn-light"}
+            width={"auto"}
+            height={"35px"}
+          />
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
