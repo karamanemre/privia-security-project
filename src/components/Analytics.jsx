6fc21bdb302aa28f06a812f90533e35f03703f30
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-04-12 15:37:50 +0300

    Added analytical component, edits were made

---
+++ src/components/Analytics.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import CornerTitle from "./CornerTitle";
+import DataAnalyticsCard from "./DataAnalyticsCard";
+import CachedIcon from "@mui/icons-material/Cached";
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+
+function Analytics() {
+  return (
+    <div className="analytics-component">
+      <div className="row">
+        <div className="col-6">
+          <DataAnalyticsCard />
+        </div>
+        <div className="col-6 d-flex flex-column">
+          <div className="d-flex">
+            <CornerTitle
+              miniTitle={"Analytics"}
+              titleone={" Analyze your data"}
+              titletwo={"with our analyze tools"}
+            />
+          </div>
+          <div className="analytics-body">
+            <p className="text-left">
+              Self-service data analytics software that lets you create visually
+              appealing data visualizations and insightful dashboards in
+              minutes.
+            </p>
+            <div className="row">
+              <div className="col-1">
+                <ContentCopyIcon />
+              </div>
+              <div className="col-11">
+                <strong>Powerful dashboard</strong>
+                <p>
+                Combine multiple reports into a single beautiful dashboard.
+                </p>
+              </div>
+              <div className="col-1">
+                <CachedIcon />
+              </div>
+              <div className="col-11">
+                <strong>Powerful dashboard</strong>
+                <p>
+                Don’t worry about the data, always be synchronized
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Analytics;
