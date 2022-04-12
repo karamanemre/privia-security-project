28003e5a169764de7b6286f845b0b939a0bc009f
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-04-12 15:42:09 +0300

    context component is enclosed in div to give border radius

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,17 @@
 import React from "react";
-import Button from "../components/Button";
 import Content from "../components/Content";
-import CornerTitle from "../components/CornerTitle";
-import DataAnalytics from "../components/DataAnalytics";
 import Header from "../components/Header";
-import MiniTitle from "../components/MiniTitle";
-import Navbar from "../components/Navbar";
-import Pricing from "../components/Pricing";
-import Title from "../components/Title";
 
 function HomePage() {
   return (
     <div className="home-page">
       <Header />
-      <Content/>
+      <div className="border-circle">
+        <div className="border">
+        <Content/>
+        </div>
+      </div>
+      
     </div>
   );
 }
