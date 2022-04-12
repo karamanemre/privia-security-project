99c09715ccfea9b1fe1c4b4b64fb82639248ef12
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-04-12 19:49:08 +0300

    content enclosed in divs

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import Analytics from "./Analytics";
+import Collaborate from "./Collaborate";
 import Features from "./Features";
+import MoreFeature from "./MoreFeature";
 
 function Content() {
   return (
     <div className="content-component container">
+      <div className="padding-bottom-10 ">
       <Features/>
+      </div>
       <hr />
+      <div className="padding-bottom-10 padding-top-10">
       <Analytics/>
+      </div>
+      <div className="padding-bottom-10 ">
+      <MoreFeature/>
+      </div>
+      <Collaborate/>
     </div>
   );
 }
