e23cb458d11ee3a1dede7fe3eb06ec1446482169
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-04-13 20:00:00 +0300

    article component created

---
+++ src/components/Article.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Article({content,title,date,fieldName,src}) {
+  return (
+    <div className="article-component d-flex justify-content-center align-items-center mb-3">
+      <div>
+        <img
+          src={src}
+          alt=""
+          width={"128px"}
+          height={"128px"}
+        />
+      </div>
+      <div className="d-flex flex-column justify-content-left body">
+        <h4>{title}</h4>
+        <p>
+         {content}
+        </p>
+        <div className="d-flex justify-content-left">
+            <strong className="text-muted">{date} - {fieldName}</strong>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Article;
