da5019119b51617994e124891b2762a2b25807d2
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-04-14 02:25:45 +0300

    scss file updated

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="App">
-      {(pathname !== "/" && pathname !== "/resources") && <Navbar />}
+      {/* {(pathname !== "/" && pathname !== "/resources") && <Navbar />} */}
       <Dashboard />
       <Footer />
     </div>
