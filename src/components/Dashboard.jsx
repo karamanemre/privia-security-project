681b8a9fa80edd706ca501401f8cee4dfc6e3e27
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-04-13 16:30:01 +0300

    new route added => path="resources"

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Route, Routes } from 'react-router'
 import HomePage from '../pages/HomePage'
+import PricingPage from '../pages/PricingPage'
+import ResourcesPage from '../pages/ResourcesPage'
 
 function Dashboard() {
   return (
-    <div>
+    <div className=''>
         <Routes>
             <Route path='/' element={<HomePage/>} />
+            <Route exact path='/pricing' element={<PricingPage/>} />
+            <Route exact path='/resources' element={<ResourcesPage/>} />
         </Routes>
     </div>
   )
