abccfbdb6907f673ca0aa2dbfe3d2b4f1571c26b
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-04-13 23:49:43 +0300

    products dropdown icon dinamic made dynamic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Button from "./Button";
 import Logo from "./Logo";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 function Navbar() {
+
+  const [isMove,setIsMove] = useState(false);
+
   return (
     <nav className="navbar d-flex justify-content-beetween container">
       <div className="logo-container">
         <Logo src={"./img/Privia_logo.png"} height={"41px"} width={"145px"} />
       </div>
       <div className="items">
-        <ul className="d-flex justify-content-center">
-          <li>About</li>
-          <li>Products</li>
+        <ul className="d-flex justify-content-center align-items-center">
+          <li className="item">About</li>
+          <li className="products-item item" onMouseOver={()=>setIsMove(true)} onMouseLeave={()=>setIsMove(false)}>
+            Products {isMove ? <KeyboardArrowDownIcon/> : <KeyboardArrowUpIcon/>} 
+            <div className="product-inner-properties">
+              <span>Analyze</span>
+              <span>Defense</span>
+              <span className="product-inner-properties-bottom">Protect</span>
+            </div>
+          </li>
           <Link to={"/pricing"}>
-            <li>Pricing</li>
+            <li className="item">Pricing</li>
           </Link>
           <Link to={"/resources"}>
-            <li>Resources</li>
+            <li className="item">Resources</li>
           </Link>
-          <li>Customers</li>
+          <li className="item">Customers</li>
         </ul>
       </div>
       <div className="button-container d-flex justify-content-center align-items-center">
