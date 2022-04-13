9853543c943a8eaa46341f67c48f89fbf9b39abd
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-04-13 16:28:37 +0300

    navbar visibility changed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import './App.css';
-import Dashboard from './components/Dashboard';
-import Footer from './components/Footer';
+import { useLocation } from "react-router";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App() {
+  const { pathname } = useLocation();
+
   return (
     <div className="App">
-      {/* <Navbar/> */}
-      <Dashboard/>
-      <Footer/>
+      {(pathname !== "/" && pathname !== "/resources") && <Navbar />}
+      <Dashboard />
+      <Footer />
     </div>
   );
 }
