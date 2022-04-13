aeed132e441adb3af713b968dc88af7192e67c33
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-04-13 16:28:49 +0300

    link added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Button from "./Button";
 import Logo from "./Logo";
 
 function Navbar() {
   return (
-    <nav className="navbar d-flex justify-content-beetween ">
+    <nav className="navbar d-flex justify-content-beetween container">
       <div className="logo-container">
         <Logo src={"./img/Privia_logo.png"} height={"41px"} width={"145px"} />
       </div>
       <div className="items">
         <ul className="d-flex justify-content-center">
-          <li>Home</li>
-          <li>Home</li>
+          <li>About</li>
+          <li>Products</li>
+          <Link to={"/pricing"}>
+            <li>Pricing</li>
+          </Link>
+          <Link to={"/resources"}>
+            <li>Resources</li>
+          </Link>
+          <li>Customers</li>
         </ul>
       </div>
       <div className="button-container d-flex justify-content-center align-items-center">
@@ -19,14 +27,14 @@ function Navbar() {
           <Button
             text={"Login"}
             className={"btn btn-link"}
-            style={{width:"auto",height:"35px"}}
+            style={{ width: "auto", height: "35px" }}
           />
         </div>
         <div className="">
           <Button
             text={"Start free trial"}
             className={"btn btn-light"}
-            style={{width:"auto",height:"35px"}}
+            style={{ width: "auto", height: "35px" }}
           />
         </div>
       </div>
